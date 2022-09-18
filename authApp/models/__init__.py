1333a16cbf34787d8e7edcfdc328d90153df657d
JohnWiJimeno <jhonwi.jimeno@gmail.com>
2022-09-18 18:12:45 -0500

    iniciando nuevamente con otra base de datos

---
+++ authApp/models/__init__.py
@@ -0,0 +1,11 @@
+from .user import User
+from .medico import Medico
+from .enfermero import Enfermero
+from .auxiliar import Auxiliar
+from .familiar import Familiar
+from .sugerencia import Sugerencia
+from .paciente import Paciente
+from .historia import Historiaclinica
+from .signosvitales import Signosvitales
+from .detallesignos import Detallesignos
+
