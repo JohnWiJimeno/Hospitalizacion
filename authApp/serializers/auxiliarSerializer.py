dda82bd32c8d09d05f169acf70f623918c1866b4
JohnWiJimeno <111402250+JohnWiJimeno@users.noreply.github.com>
2022-09-19 18:09:07 -0500

    Update auxiliarSerializer.py
    Corrigiendo user por idusuario

--- authApp/serializers/auxiliarSerializer.py
+++ authApp/serializers/auxiliarSerializer.py
@@ -5,5 +5,5 @@ from authApp.models.auxiliar import Auxiliar
 class auxiliarSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model: Auxiliar
-        fields = ['idauxiliar', 'nombre', 'apellidos', 'telefono', 'email', 'genero', 'user']
+        model = Auxiliar
+        fields = ['idauxiliar', 'nombre', 'apellido', 'correo', 'telefono','idusuario']
