aad37886aa311c7cf9be73c20748bd82f0a7f99a
JohnWiJimeno <111402250+JohnWiJimeno@users.noreply.github.com>
2022-09-19 19:45:44 -0500

    Update medicoSerializer.py
    Incial en mayuscula

--- authApp/serializers/medicoSerializer.py
+++ authApp/serializers/medicoSerializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from authApp.models.medico import medico
+from authApp.models.medico import Medico
 
 
 class medicoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Medico
-        fields = ['idmedico', 'nombre', 'apellido', 'telefono','correo', 'especialidad','idusuario']
+        fields = ['idmedico', 'nombre', 'apellidos', 'telefono','correo', 'especialidad','user']
