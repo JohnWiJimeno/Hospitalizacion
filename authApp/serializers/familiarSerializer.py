1ead15630917b1c6b9d469fbec80b46ea3e6de7d
JohnWiJimeno <111402250+JohnWiJimeno@users.noreply.github.com>
2022-09-19 19:44:31 -0500

    Update familiarSerializer.py
    inicial en mayuscula

--- authApp/serializers/familiarSerializer.py
+++ authApp/serializers/familiarSerializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from authApp.models.familiar import familiar
+from authApp.models.familiar import Familiar
 
 
 class familiarSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model: familiar
+        model: Familiar
         fields = ['idfamiliar', 'nombre', 'apellidos','parentesco', 'email','telefono']
