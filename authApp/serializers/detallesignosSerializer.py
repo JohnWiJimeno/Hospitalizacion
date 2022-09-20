374f2a68504f1069272992857a1e88d0cd9db5bf
JohnWiJimeno <111402250+JohnWiJimeno@users.noreply.github.com>
2022-09-19 19:42:57 -0500

    Update detallesignosSerializer.py
    nombres con inicial en mayuscula

--- authApp/serializers/detallesignosSerializer.py
+++ authApp/serializers/detallesignosSerializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from authApp.models.detallesignos import detallesignos
+from authApp.models.detallesignos import Detallesignos
 
 
 class detallesignosSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model: detallesignos
+        model: Detallesignos
         fields = ['iddetalle', 'idsignosvitales', 'idpaciente', 'detalle']
